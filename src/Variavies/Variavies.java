a1141ed3e56265c84aec7ed831890029e211c683
Fabiano <fabiano_slima@hotmail.com>
2023-03-01 00:24:29 -0300

    variaveis

--- src/Variavies/Variavies.java
+++ src/Variavies/Variavies.java
@@ -2,6 +2,23 @@ package Variavies;
 
 public class Variavies {
     public static void main(String[] args) {
+        double salarioMinino = 2500.33; //utilizacao do tipo double
+        //representacao dos tipos de dados
+        byte idade = 123;
+        short ano = 2021;
+        int cep = 21070333; //se começar com zero, talvez temq que ser outro tipo (String)
+        long cof = 98765432109L; //se começar com sero, talvez tenha que ser outro tipo - precisa termianr com L,
+        float pi = 3.14F; //tipo float precisa do F no final,
+        double salario = 1275.33;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14; // não pode alterar valor da variavel quando temos final
+
 
     }
 }
