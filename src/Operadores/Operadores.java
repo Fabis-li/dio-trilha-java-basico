94037454e71244b93d4f3b3d439b695329029d88
Fabiano <fabiano_slima@hotmail.com>
2023-03-02 00:07:13 -0300

    operadores

---
+++ src/Operadores/Operadores.java
@@ -0,0 +1,63 @@
+package Operadores;
+
+import java.util.Date;
+
+public class Operadores {
+    public static void main(String[] args) {
+//        String nome = "FABIANO";
+//        int idade = 22;
+//        double peso = 68.5;
+//        char sexo = 'M';
+//        boolean doadorOrgao = false;
+//        Date dataNascimento = new Date();
+//
+//        double soma = 10.5 + 15.7;
+//        int subtracao = 113 - 25;
+//        int multiplicacao = 20 * 7;
+//        int divisao = 15 / 3;
+//        int modulo =18 % 3;
+//        double resultado = (10 * 7) + (20/4);
+
+//        int numero = 5;
+//
+//        numero = -numero;
+//
+//        System.out.println(numero);
+
+//        int numero = 5;
+//
+//        numero++;
+//
+//        System.out.println(numero);
+    //ternário
+//        int a, b;
+//
+//        a = 6;
+//        b = 6;
+//
+//        String resultado = a==b ? "verdadeiro" : "falso";
+//        if(a==b)
+//            resultado = "verdadeiro";
+//        else
+//            resultado = "falso";
+
+
+
+//        System.out.println(resultado);
+
+        boolean condicao1 = true;
+
+        boolean condicao2 = false;
+
+        if(condicao1 && (7 > 4)){
+            System.out.println("as duas condições são verdadeiras");
+        }
+        if(condicao1 || condicao2){
+            System.out.println("uma das condições é verdadeira");
+        }
+
+        System.out.println("fim");
+
+
+    }
+}
