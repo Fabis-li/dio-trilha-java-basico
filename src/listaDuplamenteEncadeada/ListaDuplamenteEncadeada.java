05bd0455404e5abf381f3e88e82aa6a8847cb12b
Fabiano_silva-Lima <fabiano_slima@hotmail.com>
2023-03-20 19:32:06 -0300

    listduplamenteencadeada-metodo-get

--- src/listaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
+++ src/listaDuplamenteEncadeada/ListaDuplamenteEncadeada.java
@@ -12,9 +12,22 @@ public class ListaDuplamenteEncadeada <T> {
         this.tamanhoLista = 0;
     }
 
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private NoDuplo<T> getNo(int index){
+        NoDuplo<T> noAuxiliar = primeiroNo;
+
+        for (int i = 0; (i < index) && (noAuxiliar != null); index++) {
+            noAuxiliar = noAuxiliar.getNoProximo();
+        }
+        return noAuxiliar;
+    }
+
     public int size(){
         return tamanhoLista;
     }
 
-    
+
 }
